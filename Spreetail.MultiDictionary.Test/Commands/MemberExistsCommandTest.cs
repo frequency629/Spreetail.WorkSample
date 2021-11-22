ec593bbe84dece0ea98cf169234bb3e4c69fa218
Tim <TimSonOfSteve@gmail.com>
2021-11-21 22:20:58 -0500

    Added MemberExistsCommand

---
+++ Spreetail.MultiDictionary.Test/Commands/MemberExistsCommandTest.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using Spreetail.MultiDictionary.Commands;
+
+namespace Spreetail.MultiDictionary.Test.Commands;
+
+internal class MemberExistsCommandTest
+{
+    private TestableOutputProvider outputProvider = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        outputProvider = new TestableOutputProvider();
+    }
+
+    [Test]
+    public void Do_GivenExistingKeyAndExistingValue_OutputsTrue()
+    {
+        const string key = "foo";
+        const string value = "bar";
+
+        var mvd = new Dictionary<string, List<string>>
+        {
+            { key, new List<string>{ value } }
+        };
+
+        new MemberExistsCommand(mvd, outputProvider.Write)
+            .Do(key, value);
+
+        outputProvider.Output.Should().BeEquivalentTo("true");
+    }
+
+    [Test]
+    public void Do_GivenExistingKeyAndNonExistingValue_OutputsFalse()
+    {
+        const string key = "foo";
+        const string value = "bar";
+
+        var mvd = new Dictionary<string, List<string>>();
+
+        new MemberExistsCommand(mvd, outputProvider.Write)
+            .Do(key, value);
+
+        outputProvider.Output.Should().BeEquivalentTo("false");
+    }
+
+    [Test]
+    public void Do_GivenNonExistingKey_OutputsFalse()
+    {
+        const string key = "foo";
+        const string value = "bar";
+
+        var mvd = new Dictionary<string, List<string>>();
+
+        new MemberExistsCommand(mvd, outputProvider.Write)
+            .Do(key, value);
+
+        outputProvider.Output.Should().BeEquivalentTo("false");
+    }
+}
---
+++ Spreetail.MultiDictionary/Commands/MemberExistsCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace Spreetail.MultiDictionary.Commands;
+
+internal class MemberExistsCommand
+{
+    private readonly Dictionary<string, List<string>> dictionary;
+    private readonly Action<string> outputProvider;
+
+    public MemberExistsCommand(
+        Dictionary<string, List<string>> dictionary,
+        Action<string> outputProvider
+    )
+    {
+        this.dictionary = dictionary;
+        this.outputProvider = outputProvider;
+    }
+
+    public void Do(string key, string value) =>
+        outputProvider(
+            (
+                dictionary.ContainsKey(key) && 
+                dictionary[key].Contains(value)
+            ).ToString()
+        );
+}
