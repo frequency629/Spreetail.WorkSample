a170776396e12884fa6af16802857d196a79c93c
Tim <TimSonOfSteve@gmail.com>
2021-11-21 21:41:03 -0500

    Added RemoveCommand

---
+++ Spreetail.MultiDictionary.Test/Commands/RemoveCommandTest.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using Spreetail.MultiDictionary.Commands;
+
+namespace Spreetail.MultiDictionary.Test.Commands;
+
+internal class RemoveCommandTest
+{
+    [Test]
+    public void Do_GivenValueInKeyWithMultipleValues_RemovesValueLeavesKey()
+    {
+        const string key = "foo";
+        const string value1 = "bar";
+        const string value2 = "baz";
+
+        var mvd = new Dictionary<string, List<string>>
+        {
+            {
+                key, new List<string>
+                {
+                    value1,
+                    value2
+                }
+            }
+        };
+
+        new RemoveCommand(mvd)
+            .Do(
+                key,
+                value1
+            );
+
+        mvd.Should()
+            .ContainKey(key);
+
+        mvd[key].Should()
+            .BeEquivalentTo(new List<string>
+            {
+                value2
+            });
+    }
+
+    [Test]
+    public void Do_GivenValueInKeyWithSingleValue_RemoveEntireKey()
+    {
+        const string key = "foo";
+        const string value = "bar";
+
+        var mvd = new Dictionary<string, List<string>>
+        {
+            {
+                key, new List<string>
+                {
+                    value
+                }
+            },
+        };
+
+        new RemoveCommand(mvd)
+            .Do(
+                key,
+                value
+
+            );
+
+        mvd.Should()
+            .NotContainKey(key);
+    }
+
+    [Test]
+    public void Do_GivenKeyThatDoesNotExist_ThrowsError()
+    {
+        const string key = "foo";
+        const string value = "bar";
+        var mvd = new Dictionary<string, List<string>>();
+
+        new Action(() =>
+                new RemoveCommand(mvd)
+                    .Do(
+                        key,
+                        value
+                    )
+            )
+            .Should()
+            .ThrowExactly<Exception>();
+    }
+
+    [Test]
+    public void Do_GivenValueThatDoesNotExist_ThrowsError()
+    {
+        const string key = "foo";
+        const string value = "bar";
+
+        var mvd = new Dictionary<string, List<string>>
+        {
+            { key, new List<string>() }
+        };
+
+        new Action(() =>
+                new RemoveCommand(mvd)
+                    .Do(
+                        key,
+                        value
+                    )
+            )
+            .Should()
+            .ThrowExactly<Exception>();
+    }
+}
---
+++ Spreetail.MultiDictionary/Commands/RemoveCommand.cs
@@ -0,0 +1,42 @@
+﻿namespace Spreetail.MultiDictionary.Commands;
+
+internal class RemoveCommand
+{
+    private readonly Dictionary<string, List<string>> dictionary;
+
+    public RemoveCommand(
+        Dictionary<string, List<string>> dictionary
+    )
+    {
+        this.dictionary = dictionary;
+    }
+
+    public void Do(string key, string value)
+    {
+        if (KeyNotFound(key))
+        {
+            throw new Exception("Key does not exist");
+        }
+
+        if(ValueNotFound(key, value))
+        {
+            throw new Exception("Value does not exist");
+        }
+
+        dictionary[key].Remove(value);
+
+        if (KeyContainsNoValues(key))
+        {
+            dictionary.Remove(key);
+        }
+    }
+
+    private bool KeyContainsNoValues(string key) => !dictionary[key].Any();
+
+    private bool ValueNotFound(string key, string value) => 
+        !dictionary[key].Contains(value);
+
+    private bool KeyNotFound(string key) => 
+        !dictionary.ContainsKey(key);
+        
+}
