bbf7d5a36db28549d7d2994d6ecc38b8b61a24b6
Tim <TimSonOfSteve@gmail.com>
2021-11-21 21:44:52 -0500

    Added RemoveAllCommand

---
+++ Spreetail.MultiDictionary.Test/Commands/RemoveAllCommandTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using Spreetail.MultiDictionary.Commands;
+
+namespace Spreetail.MultiDictionary.Test.Commands;
+
+internal class RemoveAllCommandTest
+{
+
+    [Test]
+    public void Do_GivenKey_RemovesFromDictionary()
+    {
+        const string key = "foo";
+        const string value = "bar";
+
+        var mvd = new Dictionary<string, List<string>>
+        {
+            {
+                key, new List<string>
+                {
+                    value
+                }
+            },
+        };
+
+        new RemoveAllCommand(mvd)
+            .Do(key);
+
+        mvd.Should()
+            .NotContainKey(key);
+    }
+
+    [Test]
+    public void Do_GivenKeyThatDoesNotExist_ThrowsError()
+    {
+        const string key = "foo";
+
+        var mvd = new Dictionary<string, List<string>>();
+
+        new Action(() =>
+                new RemoveAllCommand(mvd)
+                    .Do(key)
+            )
+            .Should()
+            .ThrowExactly<Exception>();
+    }
+}
---
+++ Spreetail.MultiDictionary/Commands/RemoveAllCommand.cs
@@ -0,0 +1,26 @@
+﻿namespace Spreetail.MultiDictionary.Commands;
+
+internal class RemoveAllCommand
+{
+    private readonly Dictionary<string, List<string>> dictionary;
+
+    public RemoveAllCommand(
+        Dictionary<string, List<string>> dictionary
+    )
+    {
+        this.dictionary = dictionary;
+    }
+
+    public void Do(string key)
+    {
+        if (KeyNotFound(key))
+        {
+            throw new Exception("Key does not exist");
+        }
+
+        dictionary.Remove(key);
+    }
+
+    private bool KeyNotFound(string key) =>
+        !dictionary.ContainsKey(key);
+}
